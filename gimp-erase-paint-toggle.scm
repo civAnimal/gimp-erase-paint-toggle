7be9b563215c60c32d93bae1ac5c0b111b75316b
civAnimal <57868077+civAnimal@users.noreply.github.com>
2021-01-07 23:05:55 +0500

    Update gimp-erase-paint-toggle.scm

--- gimp-erase-paint-toggle.scm
+++ gimp-erase-paint-toggle.scm
@@ -15,5 +15,5 @@
 
 (script-fu-menu-register "gimp-erase-paint-toggle" "<Image>/Filters/Languages/Script-Fu")
 
-; Copyright (c) 2020 civAnimal ... Email: civanimal@gmail.com ... Twitter: civAnimal
+; Copyright (c) 2021 civAnimal ... Email: civanimal@gmail.com ... Twitter: civAnimal
 ; Released under ... GNU General Public License (v3)
